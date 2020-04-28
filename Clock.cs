937327647ab8cedb196defc47743cb787b9c756d
Adrian Infante <40429138+AdrianInfante@users.noreply.github.com>
2020-04-28 13:47:37 +0100

    Add files via upload

---
+++ Clock.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Clock : MonoBehaviour
+{
+
+
+    public bool showCurrentTime;
+    public TextMeshPro timer;
+
+
+    private void Start()
+    {
+        StartCoroutine(currentTime());
+    }
+
+
+    IEnumerator currentTime()
+    {
+        while(showCurrentTime == true)
+        {
+            timer.text = System.DateTime.Now.ToShortTimeString();
+            yield return null;
+        }
+    }
+}
---
+++ Cronometro.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Cronometro : MonoBehaviour
+{
+    public bool playCountDown;
+    public int minutes;
+    public int seconds;
+    public TextMeshPro timer;
+
+
+    private void Start()
+    {
+        StartCoroutine(countdown());
+    }
+
+
+    IEnumerator countdown()
+    {
+        while(playCountDown == true)
+        {
+            if(seconds > 0)
+            {
+                seconds--;
+                yield return new WaitForSeconds(1.0f);
+            
+            }
+            else
+            {
+                seconds = 60;
+                minutes--;
+            }
+
+            timer.text = string.Format("{00:00}:{01:00}", minutes, seconds);
+
+            if(minutes == 0 && seconds == 0)
+            {
+                playCountDown = false;
+            }
+        }
+    }
+}
